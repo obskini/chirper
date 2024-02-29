d7fc97166cd4eb9a53c99dae354f543d23564be2
obskini <84070185+obskini@users.noreply.github.com>
2024-02-29 16:51:45 +0100

    editing chirps in progress

--- chirper/app/Http/Controllers/ChirpController.php
+++ chirper/app/Http/Controllers/ChirpController.php
@@ -20,7 +20,9 @@ class ChirpController extends Controller
 
     public function index(): View
     {
-        return view('chirps.index');
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')->latest()->get(),
+        ]);
     }
 
     /**
